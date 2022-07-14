7b3b15c739cb23d250fa9e2b1dd3eabc0cc6aef5
sarojoffl <56912036+sarojoffl@users.noreply.github.com>
2022-07-14 16:04:06 +0545

    movies

--- main.py
+++ main.py
@@ -2,13 +2,33 @@ from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/{id}")
-def hello_world(id: int, name: str, dob: int):
- return {"id": id, "name": name, "Date of Birth": dob}
+movies = {
+         1:{
+            "name": "Fight Club",
+            "rating": 10,
+            "year": 1999,
+           },
 
-@app.get("/hello")
-def hello_world_2():
- return {"hello": "world2"}
+         17:{
+            "name": "Intersteller",
+            "rating": 10,
+            "year": 2014,
+           }
+         }
 
+@app.get("/movies")
+def get_all_movies():
+ return {"data": movies}
+
+@app.get("/movie/{id}")
+def get_movie_by_id(id: int):
+ return {"data": movies[id]}
+
+
+@app.post("/movie")
+def add_movie(id: int, name: str, rating:int, year:int):
+ new_movie = {id: {"name": name, "rating": rating, "year": year}}
+ movies.update(new_movie)
+ return {"data": new_movie, "message": "Update successfully"}
 
 
